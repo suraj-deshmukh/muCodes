d5747d3670a765b0a88287967b9c9236b6d08da9
Suraj Deshmukh <suraj-deshmukh@users.noreply.github.com>
2017-03-16 14:56:43 +0530

    Update classifier.r

--- ml-ui/classifier.r
+++ ml-ui/classifier.r
@@ -1,11 +1,15 @@
-get_results<-function(){
+get_results<-function(input){
     if(input$cv>1){
+        folds = createFolds(y,k=input$cv)
+        acc_matrix = NULL #confusion matrix variable
         if(identical(input$class_algo,"c_svm")){
-            folds = createFolds(y,k=input$cv)
-            c_matrix = NULL #confusion matrix variable
             for(i in folds){
-                model = svm(x[-i,])
+                model = svm(x[-i,],y[-i],cost=input$cost,kernel=input$kernel,degree=input$degree,coef0 = input$coef0)
+                pred = predict(model,x[i,])
+                table = confusionMatrix(y[i],pred)
+                acc_matrix = rbind(acc_matrix,table$overall[1:2])
             }
+            return(as.data.frame(acc_matrix))
         }
     }
     else{
